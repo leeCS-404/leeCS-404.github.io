3087e944f2f8debd7ddff5152013af7ccf4c5006
Chung Sun <chungsunlee06@gmail.com>
2025-10-14 16:05:22 +0800

    nav bar - in progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,23 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<!--Navigation bar-->
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/Blog">Blog</a>
+			</li>
+			<li>
+				<a href="/Projects">Projects</a>
+			</li>
+			<li>
+				<a href="/Contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
 </svelte:head>
 
 {@render children?.()}
