b117a6b494425ccf7fab4925406272a315d4d5be
Chung Sun <chungsunlee06@gmail.com>
2025-10-15 14:19:08 +0800

    create /build in root by run pnpm, switch the output from .svelte-kit/output/ to /build

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,11 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths:{
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
