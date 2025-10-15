85027f8a4d8b7712b8c40e8eec47f1662740df11
Chung Sun <chungsunlee06@gmail.com>
2025-10-15 14:22:16 +0800

    fix paths in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,8 @@ const config = {
 			assets: 'build',
 			fallback: '404.html'
 		}),
-		paths:{
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		paths: {
+  			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH?.replace(/\/$/, '')
 		}
 	}
 };
