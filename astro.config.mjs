4c7e4b09a50b77eefebbf2978890a1b3bf752639
Lee Chung Sun <leechungsun@Lees-MacBook-Air.local>
2025-10-20 01:39:27 +0800

    github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://leeCS-404.github.io',
     integrations: [mdx(), sitemap(), svelte()],
 });
