487c35fb0512f4d0fa810aadedeab1a151c900ca
Chung Sun <chungsunlee06@gmail.com>
2025-11-16 23:29:53 +0800

    fix dark/bright theme bug, add resources pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leeCS-404.github.io',
-    integrations: [mdx(), sitemap(), svelte()],
+  site: 'https://leeCS-404.github.io',
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
 });
